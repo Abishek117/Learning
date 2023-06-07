bc7982e45ae355e86578b2567814961303a9933f
Abishek117 <87346685+Abishek117@users.noreply.github.com>
2023-06-07 07:45:33 +0530

    updated 7-6-2023

--- JDBC/src/main/java/com/learning/main/App.java
+++ JDBC/src/main/java/com/learning/main/App.java
@@ -28,6 +28,17 @@ public class App
 		PreparedStatement pst = conn.prepareStatement("Select * from student where name = ?");
 		pst.setString(1, "manoj");
 		ResultSet rs = pst.executeQuery();
+		//System.out.println(rs.toString());
+		/*
+		   When the ResultSet object is created, it just holds a 'reference' to the actual result set data obtained from the database. 
+		   However, the data is not stored directly within the ResultSet object itself. Instead, the ResultSet object acts as a cursor or pointer to the
+		   result-set data, which is typically held on the database server or in memory buffers managed by the database driver.
+		   When you call the toString() method on the ResultSet object, it returns a string that includes the class name (com.mysql.cj.jdbc.result.ResultSetImpl)
+		   and a unique identifier (@2f465398), which is typically the memory address or some other identifier for the object.
+		   To access the actual data stored in the ResultSet, you need to use the appropriate methods provided by the JDBC API. These methods, such as getInt(), getString(), or others depending on the data types, retrieve the data from the underlying result set as you navigate through the rows using methods like next().
+		 */
+		
+	
 		while(rs.next())
 		{
 			String id = rs.getString("StudentId");
