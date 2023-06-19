1f6121816d344484effbe0bffcaa7f90c985cf74
Abishek117 <abishekramasamy2001@gmail.com>
2023-06-19 18:54:35 +0530

    June 19 -2023

--- SpringREST/src/main/java/com/example/demo/controller/DriverController.java
+++ SpringREST/src/main/java/com/example/demo/controller/DriverController.java
@@ -4,18 +4,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Drivers;
-import com.example.demo.repo.DriverRepo;
 import com.example.demo.service.DriverService;
 
 @RestController
@@ -55,11 +58,35 @@ public class DriverController
 		return service.findByAge(age);
 	}
 	
-	
 	@GetMapping(path = "/drivers/{id}")
 	public Optional<Drivers> findDriverIdandAge(@PathVariable("id") int id,@RequestParam("age") int age)
 	{
 		return service.findDriverByIdAndAge(id,age);
+	}
+	
+
+	@GetMapping(path = "/driverId/{id}")
+	@ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)  					//we can set any HttpStatus
+	public Optional<Drivers> findDriverById(@PathVariable("id") int id)
+	{
+		Optional<Drivers> driver = service.findDriverById(id);
+		return driver;
+	}
+	
+	
+	@GetMapping("/driverById/{id}")
+	public ResponseEntity<Optional<Drivers>> getDriverById(@PathVariable("id") int id) 
+	{
+		Optional<Drivers> driver = service.findDriverById(id);
+	    if (driver == null)
+	    {
+	        
+	        return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();	// Return 404 Not Found
+	    } 
+	    else 
+	    {
+	    	return ResponseEntity.ok(driver); 			// Return 200 OK with the user object
+	    }
 	}
 	
 }	
--- SpringREST/src/main/java/com/example/demo/service/DriverService.java
+++ SpringREST/src/main/java/com/example/demo/service/DriverService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.controller.DriverController;
 import com.example.demo.entity.Drivers;
 import com.example.demo.repo.DriverRepo;
 
