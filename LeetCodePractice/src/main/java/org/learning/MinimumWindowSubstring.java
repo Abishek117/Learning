ff59f944e18af4fe6b3784e32d97ff9c5318f18f
Abishek117 <87346685+Abishek117@users.noreply.github.com>
2026-02-03 23:16:01 +0530

    Added new Problem MinimumWindowSubstring

--- LeetCodePractice/src/main/java/org/learning/MinimumWindowSubstring.java
+++ LeetCodePractice/src/main/java/org/learning/MinimumWindowSubstring.java
@@ -1,5 +1,5 @@
-/** Minimum Window Substring
- *
+/**
+ * Minimum Window Substring
  * https://leetcode.com/problems/minimum-window-substring/description/
  *
  * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window.
@@ -17,18 +17,122 @@
  * Output: "a"
  * Explanation: The entire string s is the minimum window.
  *
+ *
  * Example 3:
  * Input: s = "a", t = "aa"
  * Output: ""
  * Explanation: Both 'a's from t must be included in the window.
  * Since the largest window of s only has one 'a', return empty string.
- *
  */
-
 
 
 package org.learning;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class MinimumWindowSubstring {
-    
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) {
+            return "";
+        } else if (s.length() == t.length() && s.length() == 1) {
+            if (!s.equals(t)) {
+                return "";
+            }else{
+                return s;
+            }
+        }
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char x = t.charAt(i);
+            hm.put(x, hm.getOrDefault(x, 0) + 1);
+        } //           0       8
+        //Input: s = "acbbaca", t = "aba"
+        int left = 0;
+        int right = s.length() - 1;
+        String minWindow = "";
+        int currentPointer = right;
+        String pointerInCharge = "RIGHT";
+        HashMap<Character, Integer> hm1 = new HashMap<>();
+        boolean leftBorder = false;
+        boolean rightBorder = false;
+        int bigLeft = 0;
+        int bigRight = s.length();
+//        while (left < right && left <= currentPointer) {
+        while (bigLeft < bigRight) {
+            char x = s.charAt(currentPointer);
+            if (t.indexOf(x) != -1) {
+                hm1.put(x, hm1.getOrDefault(x, 0) + 1);
+                if ("RIGHT".equals(pointerInCharge)) {
+                    if(!rightBorder){
+                        right = currentPointer;
+                        rightBorder = true;
+                    }
+                } else{
+                    if(!leftBorder){
+                        left = currentPointer;
+                        leftBorder = true;
+                    }
+                }
+            }
+            if (hm.equals(hm1)) {
+                if(minWindow.isEmpty()){
+                    minWindow = s.substring(left, s.length());
+                }
+                if ("RIGHT".equals(pointerInCharge)) {
+                    String temp = s.substring(currentPointer, right + 1);
+                    if (temp.length() < minWindow.length()) {
+                        minWindow = temp;
+                    }
+                    right = currentPointer - 1;
+                    currentPointer = left;
+                    pointerInCharge = "LEFT";
+                    bigRight--;
+                    hm1.clear();
+                } else {
+                    String temp = s.substring(left, currentPointer + 1);
+                    if (temp.length() < minWindow.length()) {
+                        minWindow = temp;
+                    }
+                    left = currentPointer;
+                    currentPointer = right;
+                    pointerInCharge = "RIGHT";
+                    bigLeft++;
+                    hm1.clear();
+                }
+            } else {
+                if ("RIGHT".equals(pointerInCharge)) {
+                    currentPointer--;
+                    bigRight--;
+                } else {
+                    currentPointer++;
+                    bigLeft++;
+                }
+            }
+        }
+        return minWindow;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(new MinimumWindowSubstring().minWindow(s, t));
+    }
+
 }
+
+
+/*
+Input
+s =
+"cabwefgewcwaefgcf"
+t =
+"cae"
+
+Use Testcase
+Output
+"aefgc"
+Expected
+"cwae"
+ */
